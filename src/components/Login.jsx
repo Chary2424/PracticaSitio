e61b5dacfeaa4233d74882e6d051596de5f07a89
Hgdv11 <140855306+Hgdv11@users.noreply.github.com>
2024-11-27 16:58:04 -0600

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import CryptoJS from 'crypto-js';
 
 const GITHUB_CLIENT_ID = 'Ov23li8nR24QsibvEM57';
-const REDIRECT_URI = 'https://practica-self.vercel.app/login';
+const REDIRECT_URI = 'https://practica-self.vercel.app/chat';
 
 /**
  * Componente de Login que permite a los usuarios iniciar sesión.
